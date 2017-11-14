ffbb6101d3ec9ee0886f02220d16edbdb5bdb817
Ryan Garcia <garciaryan007@yahoo.com>
2017-11-13 17:16:05 -0800

    middle of lesson 11

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+//Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');

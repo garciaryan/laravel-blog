491198ddd75610d94851ffa7246c6ed47bf366a9
Ryan Garcia <garciaryan007@yahoo.com>
2017-12-01 14:48:26 -0800

    finished lesson 16

--- app/Post.php
+++ app/Post.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  protected $fillable = ['title', 'body'];
-
-  public function comments() {
+  public function comments()
+  {
       return $this->hasMany(Comment::class);
+  }
+
+  public function addComment($body)
+  {
+      $this->comments()->create(compact('body'));
   }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', 'PostsController@index');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::get('/posts/create', 'PostsController@create');
 Route::post('/posts', 'PostsController@store');
+
+Route::post('/posts/{post}/comments', 'CommentsController@store');
